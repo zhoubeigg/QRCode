bf13c8f7140bf6d8ccbb0a00c98c1e3557cdab47
苏荷 <suhe@zb-mac-mini.local>
2015-10-30 11:44:42 +0800

    初始化项目

---
+++ QRCodeDemo/QRCodeDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  QRCodeDemo
+//
+//  Created by YK on 15/10/30.
+//  Copyright © 2015年 ZhouBei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ QRCodeDemo/QRCodeDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  QRCodeDemo
+//
+//  Created by YK on 15/10/30.
+//  Copyright © 2015年 ZhouBei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
